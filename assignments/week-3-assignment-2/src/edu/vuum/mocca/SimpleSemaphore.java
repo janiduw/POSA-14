f2ebf742b4fd1439629727d9f75a88944bbd2510
janiduw <janiduw@gmail.com>
2014-06-08 23:30:59 +0530

    Implemented all the todos and got the tests to work

--- assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -18,22 +18,28 @@ public class SimpleSemaphore {
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
+	private ReentrantLock mReentrantLock;
 
     /**
      * Define a Condition that waits while the number of permits is 0.
      */
     // TODO - you fill in here
+	private Condition mCondition;
 
     /**
      * Define a count of the number of available permits.
      */
     // TODO - you fill in here.  Make sure that this data member will
     // ensure its values aren't cached by multiple Threads..
+	private static int mPermits;
 
     public SimpleSemaphore(int permits, boolean fair) {
         // TODO - you fill in here to initialize the SimpleSemaphore,
         // making sure to allow both fair and non-fair Semaphore
         // semantics.
+    	mPermits = permits;
+    	mReentrantLock = new ReentrantLock(fair);
+    	mCondition = mReentrantLock.newCondition();
     }
 
     /**
@@ -42,6 +48,9 @@ public class SimpleSemaphore {
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here.
+    	while(mPermits == 0) mCondition.await();
+    	mPermits --;
+    	mReentrantLock.lockInterruptibly();
     }
 
     /**
@@ -50,6 +59,8 @@ public class SimpleSemaphore {
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here.
+    	mPermits --;
+    	mReentrantLock.lock();
     }
 
     /**
@@ -57,6 +68,8 @@ public class SimpleSemaphore {
      */
     void release() {
         // TODO - you fill in here.
+    	mPermits ++;
+    	mReentrantLock.unlock();
     }
 
     /**
@@ -65,6 +78,6 @@ public class SimpleSemaphore {
     public int availablePermits() {
         // TODO - you fill in here by changing null to the appropriate
         // return value.
-        return null;
+        return mPermits;
     }
 }
