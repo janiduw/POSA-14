0db1b7d3ad1040ce27196945876c92f7b63a1f72
janiduw <janiduw@gmail.com>
2014-06-15 12:10:15 +0530

    Submitted files

--- grading-drivers/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
+++ grading-drivers/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,4 +1,5 @@
 package edu.vuum.mocca;
+
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -11,56 +12,90 @@ import java.util.concurrent.locks.ReentrantLock;
  *        semantics, just liked Java Semaphores.
  */
 public class SimpleSemaphore {
-    /**
-     * Define a ReentrantLock to protect the critical section.
-     */
-    // TODO - you fill in here
+	/**
+	 * Define a ReentrantLock to protect the critical section.
+	 */
+	// TODO - you fill in here
+	private ReentrantLock mReentrantLock;
 
-    /**
-     * Define a Condition that waits while the number of permits is 0.
-     */
-    // TODO - you fill in here
+	/**
+	 * Define a Condition that waits while the number of permits is 0.
+	 */
+	// TODO - you fill in here
+	private Condition mCondition;
 
-    /**
-     * Define a count of the number of available permits.
-     */
-    // TODO - you fill in here. Make sure that this data member will
-    // ensure its values aren't cached by multiple Threads..
+	/**
+	 * Define a count of the number of available permits.
+	 */
+	// TODO - you fill in here. Make sure that this data member will
+	// ensure its values aren't cached by multiple Threads..
+	private volatile int mPermits;
 
-    public SimpleSemaphore(int permits, boolean fair) {
-        // TODO - you fill in here to initialize the SimpleSemaphore,
-        // making sure to allow both fair and non-fair Semaphore
-        // semantics.
-    }
+	public SimpleSemaphore(int permits, boolean fair) {
+		// TODO - you fill in here to initialize the SimpleSemaphore,
+		// making sure to allow both fair and non-fair Semaphore
+		// semantics.
+		mPermits = permits;
+		mReentrantLock = new ReentrantLock(fair);
+		mCondition = mReentrantLock.newCondition();
+	}
 
-    /**
-     * Acquire one permit from the semaphore in a manner that can be
-     * interrupted.
-     */
-    public void acquire() throws InterruptedException {
-        // TODO - you fill in here.
-    }
+	/**
+	 * Acquire one permit from the semaphore in a manner that can be
+	 * interrupted.
+	 */
+	public void acquire() throws InterruptedException {
+		// TODO - you fill in here.
+		mReentrantLock.lockInterruptibly();
+		try {
+			while (mPermits <= 0) {
+				mCondition.await();
+			}
+			--mPermits;
+		} finally {
+			mReentrantLock.unlock();
+		}
+	}
 
-    /**
-     * Acquire one permit from the semaphore in a manner that cannot be
-     * interrupted.
-     */
-    public void acquireUninterruptibly() {
-        // TODO - you fill in here.
-    }
+	/**
+	 * Acquire one permit from the semaphore in a manner that cannot be
+	 * interrupted.
+	 */
+	public void acquireUninterruptibly() {
+		// TODO - you fill in here.
+		mReentrantLock.lock();
+		try {
+			while (mPermits <= 0) {
+				mCondition.awaitUninterruptibly();
+			}
+			--mPermits;
+		} finally {
+			mReentrantLock.unlock();
+		}
+	}
 
-    /**
-     * Return one permit to the semaphore.
-     */
-    void release() {
-        // TODO - you fill in here.
-    }
+	/**
+	 * Return one permit to the semaphore.
+	 */
+	void release() {
+		// TODO - you fill in here.
+		mReentrantLock.lock();
+		try {
+			++mPermits;
+			if (mPermits > 0) {
+				mCondition.signal();
+			}
+		} finally {
+			mReentrantLock.unlock();
+		}
+	}
 
-    /**
-     * Return the number of permits available.
-     */
-    public int availablePermits() {
-        // TODO - you fill in here to return the correct result
-    	return 0;
-    }
+	/**
+	 * Return the number of permits available.
+	 */
+	public int availablePermits() {
+		// TODO - you fill in here by changing null to the appropriate
+		// return value.
+		return mPermits;
+	}
 }
